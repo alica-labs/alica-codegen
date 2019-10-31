42e22e59f5fef1bcf38294a2c88c367f504f81bd
lucasheld <lucasheld@hotmail.de>
2019-10-31 20:11:29 +0100

    added python formatter

---
+++ alica-codegen-python/README.md
@@ -0,0 +1,6 @@
+alica-codegen-python
+====================
+
+## Requirements
+- Python 3.6.4+
+- `pip install yapf`
---
+++ alica-codegen-python/src/main/java/de/unikassel/vs/alica/codegen/python/GeneratorImplPython.java
@@ -0,0 +1,33 @@
+package de.unikassel.vs.alica.codegen.python;
+
+import de.unikassel.vs.alica.codegen.GeneratorImpl;
+import de.unikassel.vs.alica.codegen.IConstraintCodeGenerator;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class GeneratorImplPython extends GeneratorImpl {
+
+    @Override
+    public void formatFile(String fileName) {
+        if (this.formatter != null && this.formatter.length() > 0) {
+            URL clangFormatStyle = GeneratorImplPython.class.getResource(".style.yapf");
+            String command = this.formatter +
+                    " --style=" + clangFormatStyle +
+                    " " + fileName;
+            try {
+                Runtime.getRuntime().exec(command).waitFor();
+            } catch (IOException | InterruptedException e) {
+                LOG.error("An error occurred while formatting generated sources", e);
+                throw new RuntimeException(e);
+            }
+        } else {
+            LOG.warn("Generated files are not formatted because no formatter is configured");
+        }
+    }
+
+    @Override
+    public IConstraintCodeGenerator getActiveConstraintCodeGenerator() {
+        return null;
+    }
+}

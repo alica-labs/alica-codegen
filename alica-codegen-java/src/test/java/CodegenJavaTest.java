a796f9cf385ca348528f22572b2a7e0e7f6546a4
lucasheld <lucasheld@hotmail.de>
2020-01-18 19:30:01 +0100

    created java codegen test project, compile and test it to test java codegen

--- alica-codegen-java/src/test/java/CodegenJavaTest.java
+++ alica-codegen-java/src/test/java/CodegenJavaTest.java
@@ -1,10 +1,7 @@
-package de.unikassel.vs.alica.codegen.java;
-
 import de.unikassel.vs.alica.codegen.CodegeneratorJava;
 import de.unikassel.vs.alica.codegen.GeneratedSourcesManagerJava;
 import de.unikassel.vs.alica.planDesigner.alicamodel.*;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.junit.*;
 import org.junit.rules.TemporaryFolder;
 
@@ -12,12 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Method;
 import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,14 +20,11 @@ public class CodegenJavaTest {
     public static TemporaryFolder tmpFolder = new TemporaryFolder();
 
     @BeforeClass
-    public static void beforeClass() throws IOException, InterruptedException {
+    public static void beforeClass() {
         tmpPath = tmpFolder.getRoot().getAbsolutePath();
-
-        generateCode();
-        compileCode();
     }
 
-    private static void generateCode() throws IOException {
+    private void generateCode() throws IOException {
         // Generates java source code
 
         Plan plan = new Plan();
@@ -75,46 +64,32 @@ public class CodegenJavaTest {
         List<Condition> conditions = new ArrayList<>();
         conditions.add(preCondition);
 
+        // generate code
+        String genPath = tmpPath + File.separator + "src" + File.separator + "main" + File.separator + "java";
         GeneratedSourcesManagerJava generatedSourcesManager = new GeneratedSourcesManagerJava();
-        generatedSourcesManager.setGenSrcPath(tmpPath);
+        generatedSourcesManager.setGenSrcPath(genPath);
         CodegeneratorJava codegenerator = new CodegeneratorJava(
                 plans,
                 behaviours,
                 conditions,
-                tmpPath,
+                genPath,
                 generatedSourcesManager);
         codegenerator.generate();
 
         // copy engine folder from resources to destination
-        URL url = CodegenJavaTest.class.getResource("engine");
+        URL url = CodegenJavaTest.class.getResource("testProject");
         String path = url.getPath();
         File engineFolderSrc = new File(path);
-        File engineFolderDst = new File(tmpPath + File.separator + "de" + File.separator + "unikassel" +
-                File.separator + "vs" + File.separator + "alica" + File.separator + "engine");
+        File engineFolderDst = new File(tmpPath);
         FileUtils.copyDirectory(engineFolderSrc, engineFolderDst);
     }
 
-    private static void compileCode() throws IOException, InterruptedException {
-        // Compiles already generated source code
-
-        // get all java files to compile
-        List<Path> paths = new ArrayList<>();
-        Files.walk(Paths.get(tmpPath))
-                .filter(Files::isRegularFile)
-                .filter(p -> FilenameUtils.getExtension(p.getFileName().toString()).equals("java"))
-                .forEach(paths::add);
-        StringBuilder stringBuilder = new StringBuilder();
-        for (Path path: paths) {
-            if (stringBuilder.length() > 0) {
-                stringBuilder.append(" ");
-            }
-            stringBuilder.append(path.toAbsolutePath().toString());
-        }
-        String filesStr = stringBuilder.toString();
-
+    private int cleanInstallProject() throws IOException, InterruptedException {
         // run compilation
-        String command = "javac " + filesStr;
-        Process process = Runtime.getRuntime().exec(command);
+        ProcessBuilder builder = new ProcessBuilder("mvn", "clean", "install");
+        builder.directory(new File(tmpPath).getAbsoluteFile());  // set current working directory for process
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
 
         // output stdout and stderr
         BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -130,10 +105,18 @@ public class CodegenJavaTest {
         // output exit code
         int exitCode = process.waitFor();
         System.out.println("Exit code: " + exitCode);
+
+        return exitCode;
     }
 
     @Test
-    public void testBehaviourCreator() {
+    public void testGeneratedCode() throws Exception {
+        generateCode();
 
+        // mvn clean install:
+        // success -> exit code 0
+        // fail -> exit code 1
+        int exitCode = cleanInstallProject();
+        Assert.assertEquals(0, exitCode);
     }
 }
---
+++ alica-codegen-java/src/test/resources/testProject/src/test/java/de/unikassel/vs/alica/codegen/java/GenCodeTest.java
@@ -0,0 +1,12 @@
+package de.unikassel.vs.alica.codegen;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class GenCodeTest {
+    @Test
+    public void testConstraintCreator() {
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
+    }
+}

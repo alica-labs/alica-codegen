0f9bf3637c4d3603e5d223fddedbb035987a1284
lucasheld <lucasheld@hotmail.de>
2020-07-03 14:29:35 +0200

    [C++] create test that use cmake to build generated code

--- alica-codegen-cpp/src/test/java/CodegenCppTest.java
+++ alica-codegen-cpp/src/test/java/CodegenCppTest.java
@@ -1,12 +1,18 @@
 import de.unikassel.vs.alica.codegen.CodegeneratorCpp;
 import de.unikassel.vs.alica.codegen.GeneratedSourcesManagerCpp;
 import de.unikassel.vs.alica.planDesigner.alicamodel.*;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,9 +27,8 @@ public class CodegenCppTest {
         tmpPath = tmpFolder.getRoot().getAbsolutePath();
     }
 
-    private void generateCode() {
-        // Generates java source code
-
+    private void generateCode() throws IOException {
+        // generates c++ source code
         Plan plan = new Plan();
         plan.setMasterPlan(true);
         EntryPoint entryPoint = new EntryPoint();
@@ -72,13 +77,47 @@ public class CodegenCppTest {
                 genPath,
                 generatedSourcesManager);
         codegenerator.generate();
+
+        // copy engine folder from resources to destination
+        URL url = CodegenCppTest.class.getResource("testProject");
+        String path = url.getPath();
+        File engineFolderSrc = new File(path);
+        File engineFolderDst = new File(tmpPath);
+        FileUtils.copyDirectory(engineFolderSrc, engineFolderDst);
+    }
+
+    private int execCommand(String... command) throws IOException, InterruptedException {
+        // exec command
+        ProcessBuilder builder = new ProcessBuilder(command);
+        builder.directory(new File(tmpPath).getAbsoluteFile());  // set current working directory for process
+        builder.redirectErrorStream(true);
+        Process process = builder.start();
+
+        // output stdout and stderr
+        BufferedReader stdout = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        BufferedReader stderr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        String s;
+        while ((s = stdout.readLine()) != null) {
+            System.out.println(s);
+        }
+        while ((s = stderr.readLine()) != null) {
+            System.out.println(s);
+        }
+
+        // output exit code
+        int exitCode = process.waitFor();
+        System.out.println("Exit code: " + exitCode);
+
+        return exitCode;
     }
 
     @Test
-    public void testGeneratedCode() {
+    public void testGeneratedCode() throws IOException, InterruptedException {
         generateCode();
-        System.out.println();
 
-        // TODO: Use test framework https://github.com/catchorg/Catch2 ?
+        // success -> exit code 0
+        // fail -> exit code != 0
+        Assert.assertEquals(0, execCommand("cmake", "."));
+        Assert.assertEquals(0, execCommand("cmake", "--build", "."));
     }
 }
